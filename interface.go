f353648c060681d25ef629adfd7b7351ad7194ca
Eric Myhre <hash@exultant.us>
2023-05-27 15:31:01 +0200

    Include an alias of the fs.Mode* constants.

--- interface.go
+++ interface.go
@@ -56,6 +56,33 @@ const (
 	O_TRUNC  int = os.O_TRUNC  // truncate regular writable file when opened.
 )
 
+// Bits used to indicate modes and permissions of files and directories.
+//
+// These are the same as the constants in the fs package,
+// and are replicated here only for convenience.
+const (
+	// The single letters are the abbreviations
+	// used by the String method's formatting.
+	ModeDir        = fs.ModeDir        // d: is a directory
+	ModeAppend     = fs.ModeAppend     // a: append-only
+	ModeExclusive  = fs.ModeExclusive  // l: exclusive use
+	ModeTemporary  = fs.ModeTemporary  // T: temporary file; Plan 9 only
+	ModeSymlink    = fs.ModeSymlink    // L: symbolic link
+	ModeDevice     = fs.ModeDevice     // D: device file
+	ModeNamedPipe  = fs.ModeNamedPipe  // p: named pipe (FIFO)
+	ModeSocket     = fs.ModeSocket     // S: Unix domain socket
+	ModeSetuid     = fs.ModeSetuid     // u: setuid
+	ModeSetgid     = fs.ModeSetgid     // g: setgid
+	ModeCharDevice = fs.ModeCharDevice // c: Unix character device, when ModeDevice is set
+	ModeSticky     = fs.ModeSticky     // t: sticky
+	ModeIrregular  = fs.ModeIrregular  // ?: non-regular file; nothing else is known about this file
+
+	// Mask for the type bits. For regular files, none will be set.
+	ModeType = fs.ModeType
+
+	ModePerm = fs.ModePerm // Unix permission bits, 0o777
+)
+
 type FSSupportingReadlink interface {
 	fs.FS
 
